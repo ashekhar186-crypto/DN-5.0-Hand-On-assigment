f6037662102bd1e6cdcac4a8ef10c07d2b30b2ba
ashekhar186-crypto <a.shekhar186@gamil.com>
2026-07-24 17:16:28 +0530

    Completed Week 3 Exercise 4 - Load Country from Spring XML

--- Week-3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week-3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,5 +1,6 @@
 package com.cognizant.spring_learn;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
+	public static void displayCountry() {
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
+
+		System.out.println(country);
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
 
 	public static void displayDate() {
 
@@ -30,5 +42,6 @@ public class SpringLearnApplication {
 		SpringApplication.run(SpringLearnApplication.class, args);
 
 		displayDate();
+		displayCountry();
 	}
 }
