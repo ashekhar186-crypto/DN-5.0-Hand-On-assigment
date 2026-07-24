478bf92c3799fd531bdd922ef4db0bb01cbd5565
ashekhar186-crypto <a.shekhar186@gamil.com>
2026-07-24 14:15:50 +0530

    Completed Week 2 Exercise 5 - Mocking and Stubbing with Multiple Returns

--- Week-2/Exercise-5-Mocking-and-Stubbing-with-Multiple-Returns/src/test/java/com/cognizant/MyServiceTest.java
+++ Week-2/Exercise-5-Mocking-and-Stubbing-with-Multiple-Returns/src/test/java/com/cognizant/MyServiceTest.java
@@ -3,7 +3,8 @@ package com.cognizant;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MyServiceTest {
 
